250d322a2b0a87c92ec97a2ad03c98b40a152f75
prabhatm09 <prabhatmishra1409@gmail.com>
2024-01-31 12:29:12 +0530

    fixed

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,7 +1,47 @@
-import React from "react";
+"use client";
+// Import necessary modules and components
+import Footer from "@/components/footer";
+import Hero from "@/components/hero";
+import Navbar from "@/components/navbar";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation"; // Use next/router instead of next/navigation
 
-function Product() {
-  return <div>page</div>;
+// Define the Layout component
+function Layout() {
+  // Initialize router
+  const router = useRouter();
+
+  // State to track user login status
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Check for the presence of window before accessing localStorage
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const storedIsLoggedIn = localStorage.getItem("isLoggedIn");
+      setIsLoggedIn(storedIsLoggedIn === "true");
+    }
+  }, []);
+
+  // Redirect to the login page if not logged in
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push("/");
+    }
+  }, [isLoggedIn, router]);
+
+  // Render the components if the user is logged in
+  return (
+    <>
+      {isLoggedIn && (
+        <>
+          <Navbar />
+          <Hero />
+          <Footer />
+        </>
+      )}
+    </>
+  );
 }
 
-export default Product;
+// Export the Layout component
+export default Layout;
